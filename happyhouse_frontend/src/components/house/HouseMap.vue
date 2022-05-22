07c5e5bbd8f3b3d26b2753b959774d0220eb9346
hyojhand <sonjw5128@gmail.com>
2022-05-22 23:43:37 +0900

    [Update] HouseMap Marker

--- happyhouse_frontend/src/components/house/HouseMap.vue
+++ happyhouse_frontend/src/components/house/HouseMap.vue
@@ -11,7 +11,7 @@
         <b-form-group v-slot="{ ariaDescribedby }">
           <b-form-checkbox-group
             style="color: black; font-weight: bold"
-            @change="selectCategory"
+            @change="selectCategory()"
             v-model="selected"
             :options="options"
             :aria-describedby="ariaDescribedby"
@@ -20,6 +20,15 @@
             size="lg"
           ></b-form-checkbox-group>
         </b-form-group>
+        <b-form-checkbox
+          style="color: black; font-weight: bold"
+          @change="changeTraffic()"
+          v-model="trafficStatus"
+          size="lg"
+        >
+          교통상황 보기
+        </b-form-checkbox>
+
         <p class="map-detail" role="button" @click="moveList()">
           아파트 상세정보 <b-icon icon="cursor"></b-icon>
         </p>
@@ -44,22 +53,27 @@ export default {
   data() {
     return {
       map: null,
-      markers: [], // 마커를 담을 배열입니다
-      markerPositions: [],
+      apartMarkers: [], // 마커를 담을 배열입니다
+      apartPositions: [],
       // placeOverlay: "", // 마커를 클릭했을 때 해당 장소의 상세정보를 보여줄 커스텀오버레이
       // contentNode: "", // 커스텀 오버레이의 컨텐츠 엘리먼트
       // currCategory: "", // 현재 선택된 카테고리를 가지고 있을 변수
-      ps: "", // 장소 검색 객체
-      infowindow: "", // 인포윈도우 객체
+
+      // infowindow: "", // 인포윈도우 객체
       categoryMarkers: [], // 선택한 카테고리 마커 담을 배열
 
       selected: [], // Must be an array reference!
       options: [
-        { text: "Cafe", value: "cafe" },
-        { text: "Bank", value: "bank" },
-        { text: "Education", value: "education" },
-        { text: "Hospital", value: "hospital" },
+        { text: "카페", value: "cafe" },
+        { text: "은행", value: "bank" },
+        { text: "교육", value: "education" },
+        { text: "병원", value: "hospital" },
+        { text: "마트", value: "mart" },
+        { text: "편의점", value: "conveni" },
+        { text: "지하철", value: "subway" },
+        { text: "문화,관광", value: "culture" },
       ],
+      trafficStatus: "", // 교통상태 확인
     };
   },
   computed: {
@@ -68,7 +82,7 @@ export default {
   async created() {
     // 아파트 좌표 리스트 받아오기
     await http.get(`/map/apt/${this.selectDong}`).then(({ data }) => {
-      this.markerPositions = data;
+      this.apartPositions = data;
     });
 
     if (!window.kakao || !window.kakao.maps) {
@@ -95,22 +109,19 @@ export default {
       // 지도 생성
       this.map = new kakao.maps.Map(container, options);
 
-      // // 인포윈도우 객체 생성
-      this.infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-
       await this.displayMarkers();
     },
     displayMarkers() {
       var bounds = new kakao.maps.LatLngBounds();
 
-      // 1. 표시되있는 marker있으면 map에서 삭제
-      if (this.markers.length > 0) {
-        this.markers.forEach((item) => {
+      // 1. 표시되있는 아파트 마커 map에서 삭제
+      if (this.apartMarkers.length > 0) {
+        this.apartMarkers.forEach((item) => {
           item.setMap(null);
         });
       }
 
-      // 1-2. 표시되어있는 카테고리 marker 삭제
+      // 1-2. 표시되어있는 카테고리 마커 삭제
       if (this.categoryMarkers.length > 0) {
         this.categoryMarkers.forEach((item) => {
           item.setMap(null);
@@ -119,35 +130,26 @@ export default {
       }
 
       // 2. 아파트 마커의 개수만큼 아파트 마커 표시
-      for (var i = 0; i < this.markerPositions.length; i++) {
-        // 2-1. 마커 이미지 세팅
+      for (var i = 0; i < this.apartPositions.length; i++) {
+        // 2-1. 아파트 마커 이미지 세팅
         var imageSrc =
-          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png";
-        var imageSize = new kakao.maps.Size(36, 37);
-        var imgOptions = {
-          spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-          spriteOrigin: new kakao.maps.Point(0, i * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
-          offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
-        };
-        var markerImage = new kakao.maps.MarkerImage(
-          imageSrc,
-          imageSize,
-          imgOptions
+          "https://cdn-icons-png.flaticon.com/512/6984/6984891.png";
+        var imageSize = new kakao.maps.Size(60, 60);
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+        // 2-2. 아파트 마커 포지션 좌표 세팅
+        var apartPosition = new kakao.maps.LatLng(
+          this.apartPositions[i].lat,
+          this.apartPositions[i].lng
         );
 
-        // 2-2. 마커 포지션 좌표 세팅
-        var markerPosition = new kakao.maps.LatLng(
-          this.markerPositions[i].lat,
-          this.markerPositions[i].lng
-        );
-
-        // 3. 마커 좌표, 이미지 세팅
+        // 3. 아파트 마커 좌표, 이미지 세팅
         var marker = new kakao.maps.Marker({
           map: this.map,
-          position: markerPosition,
+          position: apartPosition,
           image: markerImage,
         });
-        this.markers.push(marker);
+        this.apartMarkers.push(marker);
         marker.setMap(this.map);
 
         // // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우
@@ -170,38 +172,75 @@ export default {
         // });
 
         // 4. 범위 재설정을 위해 LatLngBounds 객체에 좌표를 추가해서
-        bounds.extend(markerPosition);
+        bounds.extend(apartPosition);
       }
 
       // 5. 검색된 위치들 기준으로 지도 범위 재설정
       this.map.setBounds(bounds);
     },
+    addMarker() {},
     // 카테고리 키워드 검색 후 마커찍는 메서드
     placesSearchCB(data, status) {
       if (status === kakao.maps.services.Status.OK) {
+        // // 인포윈도우 객체 생성
+        // var categoryInfowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+
         for (var i = 0; i < data.length; i++) {
           var place = data[i];
-          this.categoryMarkers = new kakao.maps.Marker({
-            // new kakao.maps.Marker({
+          var imageSrc;
+          var categoryCode = place.category_group_code;
+          if (categoryCode === "CE7") {
+            imageSrc =
+              "https://cdn-icons-png.flaticon.com/512/6984/6984949.png";
+          } else if (categoryCode === "BK9") {
+            imageSrc =
+              "https://cdn-icons-png.flaticon.com/512/6985/6985031.png";
+          } else if (
+            categoryCode === "SC4" ||
+            categoryCode === "AC5" ||
+            categoryCode === "PS3"
+          ) {
+            imageSrc =
+              "https://cdn-icons-png.flaticon.com/512/6985/6985071.png";
+          } else if (categoryCode === "HP8" || categoryCode === "PM9") {
+            imageSrc =
+              "https://cdn-icons-png.flaticon.com/512/6985/6985037.png";
+          } else if (categoryCode === "MT1") {
+            imageSrc =
+              "https://cdn-icons-png.flaticon.com/512/6984/6984975.png";
+          } else if (categoryCode === "CS2") {
+            imageSrc =
+              "https://cdn-icons-png.flaticon.com/512/6985/6985066.png";
+          } else if (categoryCode === "SW8") {
+            imageSrc =
+              "https://cdn-icons-png.flaticon.com/512/6984/6984900.png";
+          } else if (categoryCode === "CT1" || categoryCode === "AT4") {
+            imageSrc =
+              "https://cdn-icons-png.flaticon.com/512/6985/6985068.png";
+          }
+
+          var imageSize = new kakao.maps.Size(45, 45); // 마커 이미지의 크기
+          var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+          var categoryMarker = new kakao.maps.Marker({
             map: this.map,
             position: new kakao.maps.LatLng(place.y, place.x),
+            image: markerImage,
           });
 
-          // 인포윈도우
-          // 마커에 클릭이벤트를 등록합니다
-          kakao.maps.event.addListener(
-            this.categoryMarkers,
-            "click",
-            function () {
-              // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-              this.infowindow.setContent(
-                '<div style="padding:5px;font-size:12px;">' +
-                  place.place_name +
-                  "</div>"
-              );
-              this.infowindow.open(this.map, this.categoryMarkers);
-            }
-          );
+          // // // 마커에 클릭이벤트를 등록합니다
+          // kakao.maps.event.addListener(categoryMarker, "click", function () {
+          //   // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+          //   categoryInfowindow.setContent(
+          //     '<div style="padding:5px;font-size:12px;">' +
+          //       place.place_name +
+          //       "</div>"
+          //   );
+          //   categoryInfowindow.open(this.map, categoryMarker);
+          // });
+
+          categoryMarker.setMap(this.map);
+          this.categoryMarkers.push(categoryMarker);
         }
       }
     },
@@ -210,62 +249,82 @@ export default {
         this.categoryMarkers.forEach((item) => {
           item.setMap(null);
         });
-        this.categoryMarkers = null;
       }
 
-      console.log(this.selected);
-      console.log(this.selected.length);
-      console.log(this.categoryMarkers);
-
       // 장소 검색 객체
-      this.ps = new kakao.maps.services.Places(this.map);
+      var ps = new kakao.maps.services.Places(this.map);
 
       for (var k = 0; k < this.selected.length; k++) {
-        if (this.selected[k] == "cafe") {
-          // 카페
-          this.ps.categorySearch("CE7", this.placesSearchCB, {
+        // 카페
+        if (this.selected[k] === "cafe") {
+          ps.categorySearch("CE7", this.placesSearchCB, {
             useMapBounds: true,
           });
-        } else if (this.selected[k] == "bank") {
-          // 은행
-          this.ps.categorySearch("BK9", this.placesSearchCB, {
+        }
+        // 은행
+        else if (this.selected[k] === "bank") {
+          ps.categorySearch("BK9", this.placesSearchCB, {
             useMapBounds: true,
           });
-        } else if (this.selected[k] == "education") {
-          //학교, 학원, 어린이집/유치원
-          this.ps.categorySearch("SC4", this.placesSearchCB, {
+        }
+        //학교, 학원, 어린이집/유치원
+        else if (this.selected[k] === "education") {
+          ps.categorySearch("SC4", this.placesSearchCB, {
             useMapBounds: true,
           });
-          this.ps.categorySearch("AC5", this.placesSearchCB, {
+          ps.categorySearch("AC5", this.placesSearchCB, {
             useMapBounds: true,
           });
-          this.ps.categorySearch("PS3", this.placesSearchCB, {
+          ps.categorySearch("PS3", this.placesSearchCB, {
             useMapBounds: true,
           });
-        } else if (this.selected[k] == "hospital") {
-          // 병원, 약국
-          this.ps.categorySearch("HP8", this.placesSearchCB, {
+        }
+        // 병원, 약국
+        else if (this.selected[k] === "hospital") {
+          ps.categorySearch("HP8", this.placesSearchCB, {
             useMapBounds: true,
           });
-          this.ps.categorySearch("PM9", this.placesSearchCB, {
+          ps.categorySearch("PM9", this.placesSearchCB, {
+            useMapBounds: true,
+          });
+        }
+        // 마트
+        else if (this.selected[k] === "mart") {
+          ps.categorySearch("MT1", this.placesSearchCB, {
+            useMapBounds: true,
+          });
+        }
+        // 편의점
+        else if (this.selected[k] === "conveni") {
+          ps.categorySearch("CS2", this.placesSearchCB, {
+            useMapBounds: true,
+          });
+        }
+        // 지하철
+        else if (this.selected[k] === "subway") {
+          ps.categorySearch("SW8", this.placesSearchCB, {
+            useMapBounds: true,
+          });
+        }
+        // 문화,관광
+        else if (this.selected[k] === "culture") {
+          ps.categorySearch("CT1", this.placesSearchCB, {
+            useMapBounds: true,
+          });
+          ps.categorySearch("AT4", this.placesSearchCB, {
             useMapBounds: true,
           });
         }
       }
 
-      /** 고민할것 */
-      // CS2 편의점
-      // CT1 문화시설
-
-      // MT1 대형마트
-      // PK6 주차장
-      // OL7 주유소, 충전소
-      // SW8 지하철역
-      // AG2 중개업소
-      // PO3 공공기관
-      // AT4 관광명소
-      // AD5 숙박
-      // FD6 음식점
+      // console.log(this.categoryMarkers);
+    },
+    changeTraffic() {
+      if (this.trafficStatus) {
+        this.map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+      } else {
+        this.map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+      }
     },
 
     moveList() {
